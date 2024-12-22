84f6497b8217eed8a1465c459979c613cc68baf9
Geerthiga18 <156160909+Geerthiga18@users.noreply.github.com>
2024-12-22 12:56:34 +0530

    Update productController.js
    modified file

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,32 +1,31 @@
-const productModel = require("../models/productModel");
+const ProductModel = require('../models/productModel');
 
-
-
-// Get products API - /api/v1/products
+//Get Products API - /api/v1/products
 exports.getProducts = async (req, res, next) => {
-    const products = await productModel.find({});
+    const query = req.query.keyword?{ name : { 
+        $regex: req.query.keyword, //search using regular expression
+        $options: 'i'     //for case sensitivity can give upper or lower case
+     }}:{} // if it is not found empty 
+
+    const products = await ProductModel.find(query);
     res.json({
         success: true,
         products
-    });
-};
+    })
+}
 
-// Get single product API - /api/v1/product/:id
-exports.getSingleProducts = async (req, res, next) => {
-    try{
-        const product = await productModel.findById(req.params.id);
+//Get Single Product API - /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+    try {
+        const product = await ProductModel.findById(req.params.id);
         res.json({
             success: true,
             product
         })
-    }
-    catch(error){
+    } catch (error) {
         res.status(404).json({
             success: false,
-            message: 'Unable to get product with that ID' 
+            message: 'Unable to get Product with that ID'
         })
-        
     }
-   
-    
-};
+}
