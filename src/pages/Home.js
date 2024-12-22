2cf0622e0a177ad1fa1ddf4beed29d5a0018c0f4
Geerthiga18 <geerthishan@gmail.com>
2024-12-22 12:38:57 +0530

    Header.js, Footer.js, ProductCard.js, Search.js files added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
 import {Fragment, useEffect, useState} from 'react'
+import ProductCard from '../components/ProductCard'
+import { useSearchParams } from 'react-router-dom';
 
 export default function Home() {
-    
+    const [products, setProducts] = useState([]);
+    const [searchParams, setSearchParams] =  useSearchParams()
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL+'/products?'+searchParams)
+        .then(res => res.json())
+        .then( res => setProducts(res.products))
+    },[searchParams])      //whenever searchparams value is changed by enter the item in search bar, the use effect will be called and that will be shown on page
+
     return <Fragment>
         <h1 id="products_heading">Latest Products</h1>
 
