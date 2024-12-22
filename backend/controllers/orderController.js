61e98e3bf9aa0dfccdc3c8a1dcfa6ea9c559ad48
Geerthiga18 <156160909+Geerthiga18@users.noreply.github.com>
2024-12-22 12:57:40 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,5 @@
 const orderModel = require('../models/orderModel');
+const productModel = require('../models/productModel');
 
 //Create Order - /api/v1/order 
 exports.createOrder = async (req, res, next) => {
@@ -6,7 +7,13 @@ exports.createOrder = async (req, res, next) => {
     const amount = Number(cartItems.reduce((acc, item) => (acc + item.product.price * item.qty), 0)).toFixed(2);
     const status = 'pending';
     const order = await orderModel.create({cartItems, amount, status})
-//create order
+
+    // Updating product stock
+    cartItems.forEach(async (item)=> {
+        const product = await productModel.findById(item.product._id);
+        product.stock = product.stock - item.qty;
+        await product.save();
+    })
 
 
     res.json(
