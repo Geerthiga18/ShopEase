03c4016fa12421a743fd39d5debcec5a1bf64af9
Geerthiga18 <156160909+Geerthiga18@users.noreply.github.com>
2024-12-25 14:46:06 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,28 +2,27 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const path = require('path');
+const cors = require('cors');
 const connectDatabase = require('./config/connectDatabase');
+dotenv.config({path: path.join(__dirname, 'config', 'config.env')})
 
-// Load environment variables
-dotenv.config({ path: path.join(__dirname, 'config', 'config.env') });
-
-// Body parser middleware to handle JSON requests
-app.use(express.json());
-
-// Import routes
 const products = require('./routes/product');
 const orders = require('./routes/order');
 
-
 connectDatabase();
 
+app.use(express.json());
+app.use(cors());
+app.use('/api/v1/',products);
+app.use('/api/v1/',orders);
 
-// Use routes
-app.use(express.json())
-app.use('/api/v1/products', products);  // Matches '/api/v1/products'
-app.use('/api/v1/orders', orders);      // Matches '/api/v1/orders'
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'frontend',  'build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
+    });
+}
 
-// Start the server
 app.listen(process.env.PORT, () => {
-    console.log(`Server listening on Port ${process.env.PORT} in ${process.env.NODE_ENV}`);
+    console.log(`Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`)
 });
